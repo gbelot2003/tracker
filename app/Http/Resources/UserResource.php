d890174aa51c6dbf35d2628e0578a76f1665b048
Gerardo Belot <gerardo.belot@gmail.com>
2023-03-02 07:57:32 -0600

    Corrigiendo parametro status

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -18,7 +18,7 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
-            'state' => $this->state,
+            'status' => $this->status,
         ];
     }
 }
